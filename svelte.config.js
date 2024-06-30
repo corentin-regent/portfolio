137092e4151257ac1cb24d78317b1a74dac5504a
Corentin Régent <corentin.regent@gmail.com>
2024-06-30 07:17:51 +0000

    Enable offline precompression & remove html minifier

--- svelte.config.js
+++ svelte.config.js
@@ -1,32 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import htmlMinifierAdaptor from "sveltekit-html-minifier";
 
 export default {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: htmlMinifierAdaptor(adapter({
+    adapter: adapter({
       fallback: '404.html',
-      pages: 'build'
-    }), {
       pages: 'build',
-      minifierOptions: {
-        collapseBooleanAttributes: true,
-        collapseInlineTagWhitespace: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true,
-        removeAttributeQuotes: true,
-        removeComments: true,
-        removeEmptyAttributes: true,
-        removeOptionalTags: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        sortAttributes: true,
-        sortClassName: true,
-        useShortDoctype: true,
-      }
+      precompress: true,
     }),
     alias: {
       '$components/*': 'src/components/*',
