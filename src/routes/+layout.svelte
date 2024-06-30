07c2163600ce9e0245d0cffd3bae95974c90fbaa
Corentin Régent <corentin.regent@gmail.com>
2024-06-30 06:54:55 +0000

    Add canonical link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,6 +25,7 @@
   <meta name="color-scheme" content="dark light" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+  <link rel="canonical" href={$resolve({absolute: true})} />
   {#each availableLanguages as language}
     <link rel="alternate" hreflang={language} href={$resolve({ language, absolute: true })} />
   {/each}
