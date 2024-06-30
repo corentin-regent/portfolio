294588fb18650bf5f449a9ef136bb4e8e009f460
Corentin Régent <corentin.regent@gmail.com>
2024-06-30 05:50:54 +0000

    Add `<html lang="%lang%">`

---
+++ src/hooks.server.js
@@ -0,0 +1,11 @@
+import availableLanguages from '$stores/language/available.js';
+import defaultLanguage from '$stores/language/default.js';
+
+export function handle({ event, resolve }) {
+  return resolve(event, {
+    transformPageChunk: ({ html }) => {
+      const selectedLanguage = availableLanguages.find(lang => event.url.pathname.startsWith(`/${lang}/`)) ?? defaultLanguage
+      return html.replace('%lang%', selectedLanguage)
+    }
+  });
+}
