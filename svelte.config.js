c28d38a1992bf955605cd0220443c400dc5d0c0c
corentin-regent <corentin-regent@users.noreply.github.com>
2024-07-04 07:04:57 +0000

    Lint codebase [skip ci]

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,36 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import htmlMinifierAdaptor from "sveltekit-html-minifier";
+import htmlMinifierAdaptor from 'sveltekit-html-minifier';
 
 export default {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: htmlMinifierAdaptor(adapter({
-      fallback: '404.html',
-      pages: 'build'
-    }), {
-      pages: 'build',
-      minifierOptions: {
-        collapseBooleanAttributes: true,
-        collapseInlineTagWhitespace: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true,
-        removeAttributeQuotes: true,
-        removeComments: true,
-        removeEmptyAttributes: true,
-        removeOptionalTags: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        sortAttributes: true,
-        sortClassName: true,
-        useShortDoctype: true,
+    adapter: htmlMinifierAdaptor(
+      adapter({
+        fallback: '404.html',
+        pages: 'build',
+      }),
+      {
+        pages: 'build',
+        minifierOptions: {
+          collapseBooleanAttributes: true,
+          collapseInlineTagWhitespace: true,
+          collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          sortAttributes: true,
+          sortClassName: true,
+          useShortDoctype: true,
+        },
       }
-    }),
+    ),
     alias: {
       '$components/*': 'src/components/*',
       '$messages/*': 'src/messages/*',
