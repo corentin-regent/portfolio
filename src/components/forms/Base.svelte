8f0d1a6e0a951e0e9f8a3a35dc946b9fe8e50b0c
Corentin Régent <corentin.regent@gmail.com>
2024-07-07 07:00:25 +0000

    Don't render spinning loading icon when not loading

--- src/components/forms/Base.svelte
+++ src/components/forms/Base.svelte
@@ -8,6 +8,7 @@
 <script>
   const { children, class: cls, submitMessageId, ...restProps } = $props();
   let isSending = $state(false);
+  let showLoadingIcon = $state(false);
 </script>
 
 <form
@@ -15,23 +16,28 @@
   inert={isSending}
   class={classes(cls, 'grid grid-cols-1')}
   use:enhance={() => {
-    isSending = true;
+    // use timeouts to support animations
+    showLoadingIcon = true;
+    setTimeout(() => (isSending = true));
     return async ({ update }) => {
       await update();
       isSending = false;
+      setTimeout(() => (showLoadingIcon = false), 500);
     };
   }}
 >
   {@render children()}
   <div class="grid grid-cols-5 mt-4">
-    <div class={classes('duration-500', isSending ? 'opacity-100' : 'opacity-0')}>
-      <Favicon class="animate-spin-non-uniform w-10 mx-4 ml-auto" />
-    </div>
+    {#if showLoadingIcon}
+      <div class={classes('duration-500', isSending ? 'opacity-100' : 'opacity-0')}>
+        <Favicon class="animate-spin-non-uniform w-10 mx-4 ml-auto" />
+      </div>
+    {/if}
     <input
       type="submit"
       value={$messages.get(submitMessageId)}
       class={classes(
-        'cursor-pointer rounded border border-bg3 col-span-3 leading-10',
+        'cursor-pointer rounded border border-bg3 col-start-2 col-span-3 leading-10',
         'uppercase font-bold text-text-light animated-primary-bg'
       )}
     />
