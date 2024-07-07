7f8e4a968c01165693b227199b9463358f561887
Corentin Régent <corentin.regent@gmail.com>
2024-07-07 06:59:25 +0000

    Removed broken email regex

--- src/components/inputs/Email.svelte
+++ src/components/inputs/Email.svelte
@@ -1,20 +1,10 @@
 <script context="module">
   import Input from '$components/inputs/Base.svelte';
   import messages from '$stores/i18n/messages.svelte.js';
-
-  // Taken from https://html.spec.whatwg.org/multipage/input.html#email-state-(type=email)
-  const emailPattern =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 </script>
 
 <script>
   let { value = $bindable(), ...restProps } = $props();
 </script>
 
-<Input
-  {...restProps}
-  type="email"
-  placeholder={$messages.get('email-placeholder')}
-  pattern={emailPattern}
-  bind:value
-/>
+<Input {...restProps} type="email" placeholder={$messages.get('email-placeholder')} bind:value />
