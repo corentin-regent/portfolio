3d7e1c422b007ab1c09d2f5fde6c20067a28a49a
Corentin Régent <corentin.regent@gmail.com>
2024-07-07 10:18:52 +0000

    Revert "Don't throw anymore if message is not found"
    This reverts commit 9558f2980ee108bc93717f4b895ff5ce7397d942.

--- src/stores/i18n/messages.svelte.js
+++ src/stores/i18n/messages.svelte.js
@@ -10,7 +10,13 @@ export default derived(namespaces, $namespaces => {
   return {
     get(messageId, interpolated) {
       const message = availableMessages[messageId];
-      return interpolated ? message(interpolated) : message;
+      const ret = interpolated ? message(interpolated) : message;
+      if (ret === undefined) {
+        throw new Error(
+          `Message '${messageId}' not found in namespaces: ${$namespaces.map(namespace => `'${namespace}'`).join(', ')}`
+        );
+      }
+      return ret;
     },
   };
 });
