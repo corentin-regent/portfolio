ccf64cf8983584bc87cbb1a41308781de3101bfd
Corentin Régent <corentin.regent@gmail.com>
2024-07-07 12:03:39 +0000

    Fix basePath once and for all

--- src/stores/language/selected.svelte.js
+++ src/stores/language/selected.svelte.js
@@ -1,9 +1,7 @@
 import { page } from '$app/stores';
 import { derived } from 'svelte/store';
-import availableLanguages from './available.js';
 import defaultLanguage from './default.js';
 
 export default derived(page, $page => {
-  const firstUrlPart = $page.url.pathname.split('/')[1];
-  return availableLanguages.includes(firstUrlPart) ? firstUrlPart : defaultLanguage;
+  return $page.params.language ?? defaultLanguage;
 });
--- src/stores/navigation/resolve.svelte.js
+++ src/stores/navigation/resolve.svelte.js
@@ -1,25 +1,26 @@
-import { base as basePath } from '$app/paths';
 import { page } from '$app/stores';
 import defaultLanguage from '$stores/language/default.js';
 import selectedLanguage from '$stores/language/selected.svelte.js';
 import withTrailingSlash from '$utils/withTrailingSlash.js';
 import { derived } from 'svelte/store';
 
-const origin = 'https://corentin-regent.github.io/portfolio';
+const origin = 'https://corentin-regent.github.io';
 
 export default derived([page, selectedLanguage], ([$page, $selectedLanguage]) => {
+  const basePath = $page.url.pathname.includes('/portfolio') ? '/portfolio' : '';
   return ({
     endpoint = $page.url.pathname,
     language = $selectedLanguage,
     absolute = false,
     includeDefaultLanguage = false,
   }) => {
-    const endpointWithoutLanguage = endpoint.replace(new RegExp(`^/${$selectedLanguage}/`), '/');
+    const endpointWithoutBasePath = basePath ? endpoint.replace(new RegExp(`^${basePath}`), '') : endpoint
+    const endpointWithoutLanguage = endpointWithoutBasePath.replace(new RegExp(`^/${$selectedLanguage}/`), '/')
     const includeLanguage = includeDefaultLanguage || language !== defaultLanguage;
     const endpointWithLanguage = includeLanguage
       ? `/${language}${endpointWithoutLanguage}`
       : endpointWithoutLanguage;
-    const endpointIncludingBase = basePath.startsWith('/') ? basePath + endpointWithLanguage : endpointWithLanguage
-    return withTrailingSlash(absolute ? origin + endpointIncludingBase : endpointIncludingBase);
+    const endpointWithBasePath = basePath + endpointWithLanguage;
+    return withTrailingSlash(absolute ? origin + endpointWithBasePath : endpointWithBasePath);
   };
 });
