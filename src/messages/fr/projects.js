60ff2395e90b6aaad97e8ef9d90cdb092fef09ab
corentin-regent <corentin-regent@users.noreply.github.com>
2024-07-20 17:37:22 +0000

    Lint codebase [skip ci]

--- src/messages/fr/projects.js
+++ src/messages/fr/projects.js
@@ -14,32 +14,41 @@ const pageMessages = {
 
 const rateControl = {
   'rate-control': 'rate-control',
-  'rate-control-text': "De nombreuses API externes appliquent des limites de débit et renvoient des erreurs si elles sont requêtées trop fréquemment. Cette librairie est une solution efficiente pour la limitation et la planification des requêtes en Python. Divers algorithmes sont disponibles, de même que de nombreuses fonctionnalités telles que la synchronisation des requêtes et leur priorisation.",
+  'rate-control-text':
+    'De nombreuses API externes appliquent des limites de débit et renvoient des erreurs si elles sont requêtées trop fréquemment. Cette librairie est une solution efficiente pour la limitation et la planification des requêtes en Python. Divers algorithmes sont disponibles, de même que de nombreuses fonctionnalités telles que la synchronisation des requêtes et leur priorisation.',
 };
 
 const poetrel = {
-  'poetrel': 'Poetrel',
-  'poetrel-text-until-github-actions': 'Poetrel est un ensemble de pipelines de livraison continue pour ',
-  'poetrel-text-until-pypi': ". Je l'ai initialement développé pour la montée de version automatisée de projets Python et leur publication sur ",
-  'poetrel-text-until-github-marketplace': ", tout en gérant également les notes de version et le fichier Changelog. Par ailleurs, Poetrel s'auto-emploie pour sa propre livraison continue sur ",
+  poetrel: 'Poetrel',
+  'poetrel-text-until-github-actions':
+    'Poetrel est un ensemble de pipelines de livraison continue pour ',
+  'poetrel-text-until-pypi':
+    ". Je l'ai initialement développé pour la montée de version automatisée de projets Python et leur publication sur ",
+  'poetrel-text-until-github-marketplace':
+    ", tout en gérant également les notes de version et le fichier Changelog. Par ailleurs, Poetrel s'auto-emploie pour sa propre livraison continue sur ",
 };
 
 const assemblife = {
-  'assemblife': "Le Jeu de la Vie de Conway en Assembleur",
-  'assemblife-text': "Cherchant à mieux comprendre les mécanismes de bas niveau dans les ordinateurs, et à être capable d'effectuer des micro-optimisations sur des applications critiques, je développe actuellement le Jeu de la Vie en utilisant le langage Assembleur. Mon programme est conçu pour l'architecture x86-64, sur Linux, et utilise VGA pour afficher la grille dans une interface graphique.",
+  assemblife: 'Le Jeu de la Vie de Conway en Assembleur',
+  'assemblife-text':
+    "Cherchant à mieux comprendre les mécanismes de bas niveau dans les ordinateurs, et à être capable d'effectuer des micro-optimisations sur des applications critiques, je développe actuellement le Jeu de la Vie en utilisant le langage Assembleur. Mon programme est conçu pour l'architecture x86-64, sur Linux, et utilise VGA pour afficher la grille dans une interface graphique.",
 };
 
 const portfolio = {
-  'portfolio': 'Ce site portfolio',
-  'portfolio-text-until-github-pages': "Ce projet a été l'occasion de me charger de l'intégralité d'un processus de développement web, de la conception de l'interface utilisateur au déploiement automatique du site sur ",
-  'portfolio-text-after-github-pages': ". J'ai également acquis davantage d'expérience avec les enjeux de performance, d'accessibilité et de référencement, qui sont une priorité absolue pour toute application web publique.",
+  portfolio: 'Ce site portfolio',
+  'portfolio-text-until-github-pages':
+    "Ce projet a été l'occasion de me charger de l'intégralité d'un processus de développement web, de la conception de l'interface utilisateur au déploiement automatique du site sur ",
+  'portfolio-text-after-github-pages':
+    ". J'ai également acquis davantage d'expérience avec les enjeux de performance, d'accessibilité et de référencement, qui sont une priorité absolue pour toute application web publique.",
 };
 
 const msgspec = {
-  'msgspec': 'msgspec',
-  'msgspec-text-until-pr': "msgspec est la librairie la plus performante et économe en mémoire pour la sérialisation JSON dans l'écosystème Python. Le projet est écrit en C, hautement optimisé, et interagit directement avec l'API C de Python. Dans ",
+  msgspec: 'msgspec',
+  'msgspec-text-until-pr':
+    "msgspec est la librairie la plus performante et économe en mémoire pour la sérialisation JSON dans l'écosystème Python. Le projet est écrit en C, hautement optimisé, et interagit directement avec l'API C de Python. Dans ",
   'this-pr': 'cette Pull Request',
-  'msgspec-text-after-pr': ", j'ai implémenté une fonctionnalité pour imposer des contraintes de validation lors de la désérialisation de nombres de précision arbitraire.",
+  'msgspec-text-after-pr':
+    ", j'ai implémenté une fonctionnalité pour imposer des contraintes de validation lors de la désérialisation de nombres de précision arbitraire.",
 };
 
 export default {
