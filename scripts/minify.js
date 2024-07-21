a1bd187e9bc5f5f7ebff3e21973af0af55d262e5
Corentin Régent <corentin.regent@gmail.com>
2024-07-21 17:52:24 +0000

    Manually minify HTML and SVG files
    No minification: 572Ko
    sveltekit-html-minifier: 560Ko
    Now: 540Ko

---
+++ scripts/minify.js
@@ -0,0 +1,37 @@
+import { readdir, readFile, writeFile } from 'fs/promises';
+import { minify as minifyHtml } from 'html-minifier';
+import { optimize as minifySvg } from 'svgo';
+
+const minifyHtmlConfig = {
+  collapseBooleanAttributes: true,
+  collapseInlineTagWhitespace: true,
+  collapseWhitespace: true,
+  minifyJS: true,
+  removeAttributeQuotes: true,
+  removeComments: true,
+  removeEmptyAttributes: true,
+  removeOptionalTags: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  sortAttributes: true,
+  sortClassName: true,
+  useShortDoctype: true,
+};
+
+readdir('build/', { withFileTypes: true, recursive: true }).then(dirents =>
+  dirents
+    .filter(dirent => dirent.isFile())
+    .filter(entry => entry.name.endsWith('.html') || entry.name.endsWith('.svg'))
+    .map(entry => ({
+      path: `${entry.parentPath}/${entry.name}`,
+      minify: entry.name.endsWith('.html')
+        ? content => minifyHtml(content, minifyHtmlConfig)
+        : content => minifySvg(content).data,
+    }))
+    .map(({ path, minify }) =>
+      readFile(path, 'utf-8')
+        .then(minify)
+        .then(minified => writeFile(path, minified, 'utf-8'))
+    )
+);
