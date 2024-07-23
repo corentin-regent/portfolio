746031fdef7b817cd1455fa9fe08a1e80fce4eb6
Corentin Régent <corentin.regent@gmail.com>
2024-07-23 07:47:23 +0000

    Generate lighthouse URLs in the CI to workaround weird bugs

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,17 +1,3 @@
-import { readdirSync } from 'fs';
-import availableLanguages from './src/stores/language/available.js';
-import defaultLanguage from './src/stores/language/default.js';
-
-const allUrls = readdirSync('src/routes/', { recursive: true })
-  .filter(path => path.endsWith('+page.svelte'))
-  .map(path => path.replace('+page.svelte', ''))
-  .flatMap(route =>
-    route.includes('[[language=language]]')
-      ? availableLanguages.map(language => route.replace('[[language=language]]/', language === defaultLanguage ? '' : language + '/'))
-      : [route]
-  )
-  .map(route => 'http://localhost/' + route)
-
 export default {
   ci: {
     collect: {
@@ -20,7 +6,6 @@ export default {
       additive: false,
       numberOfRuns: 3,
       startServerCommand: 'docker run --rm -p 80:80 portfolio',
-      url: allUrls,
     },
     assert: {
       preset: "lighthouse:all",
