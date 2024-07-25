7a96faae37e88ba49f92e443590b0250ce6fe446
Corentin Régent <corentin.regent@gmail.com>
2024-07-25 04:35:09 +0000

    Remove useless configs

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,15 +1,11 @@
 export default {
   launch: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     dumpio: true,
     headless: true,
   },
   server: {
     command: 'npm run dev',
+    host: 'localhost',
     port: 5173,
-    debug: true,
-    launchTimeout: 30 * 1000,
-    protocol: 'http',
-    host: 'localhost'
   },
 };
