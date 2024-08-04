5ccef1b87cbf850e5bb20751de56e43f9a1a6b0b
Corentin Régent <corentin.regent@gmail.com>
2024-08-04 14:08:39 +0000

    Added Assemblife demo

--- src/components/embeds/Assemblife.svelte
+++ src/components/embeds/Assemblife.svelte
@@ -1,11 +1,10 @@
 <script context="module">
-  import EmbeddedIcon from '$components/embeds/Icon.svelte';
+  import assemblife from '$assets/assemblife.webp';
+  import Image from '$components/Image.svelte';
 </script>
 
 <script>
   const { ...restProps } = $props();
 </script>
 
-<EmbeddedIcon {...restProps}>
-  <span class="font-bold text-xl text-white whitespace-nowrap -mx-4">Work in progress</span>
-</EmbeddedIcon>
+<Image {...restProps} src={assemblife} alt="Assemblife" />
