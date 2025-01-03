ab8b8c8e7e3cc9b15ef396e3fe424479937cd6cb
Corentin Régent <corentin.regent@gmail.com>
2025-01-03 10:09:54 +0000

    Move already compressed assemblife webp to static folder

--- src/components/embeds/Assemblife.svelte
+++ src/components/embeds/Assemblife.svelte
@@ -1,5 +1,4 @@
 <script module>
-  import assemblife from '$assets/assemblife.webp';
   import Image from '$components/Image.svelte';
 </script>
 
@@ -7,4 +6,4 @@
   const { ...restProps } = $props();
 </script>
 
-<Image {...restProps} src={assemblife} alt="Assemblife" />
+<Image {...restProps} src="/assemblife.webp" alt="Assemblife" />
